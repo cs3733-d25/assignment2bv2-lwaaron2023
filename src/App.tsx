fbc864b6261e7425c0760f801209aa6ec158ede5
lwaaron2023 <alwaller@wpi.edu>
2025-03-29 17:12:18 -0400

    Adding functionality to files and removing unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ export default function App() {
     return (
     <div className={"app"} id = "root">
         <Title className={"CS3733-D25"} teamNumber = {68} hobbies = {["Refurbishing Computers For Charity","FILL ME IN"]}></Title>
+        <></>
     </div>
     )
 }
