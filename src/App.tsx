27780f4dbb0c6688bad05917c7ba7eee858af43e
lwaaron2023 <alwaller@wpi.edu>
2025-03-29 17:11:14 -0400

    Getting the project to have the necessary files to begin development

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import {Todo} from "./types.ts"
-import {TodoForm} from "./todo-form.tsx";
-import {TodoList} from "./todo-list.tsx";
+import Title from "./title.tsx";
+
+
+
 
 export default function App() {
-    const [todos, setTodos] = useState<Todo[]>([]); //<> tells useState that the type of array is Todo
-    const generatedKeys = new Set();
-    const maxTasks = 1000;
-    const [curTasks, setCurTasks] = useState(0);
-    function handleAdd(description: string){
-        if(curTasks < maxTasks) {
-            const tempID = generateID();
-            const newTodo: Todo = {
-                id: tempID,
-                description: description,
-                completed: false
-            }
-            setCurTasks(curTasks + 1);
-            setTodos([...todos, newTodo])
-            generatedKeys.add(tempID);
-        }
-    }
 
-    function generateID(){
-        let temp = Math.random()*maxTasks
-        while(generatedKeys.has(temp)){
-            if(temp < maxTasks-1){
-                temp++
-            }
-            else{
-                temp = 0
-            }
-        }
-        return temp
-    }
-
-    function handleDelete(id: number){
-        if(curTasks > 0) {
-            setTodos(todos.filter(todo => todo.id !== id))
-            setCurTasks(curTasks - 1)
-            generatedKeys.delete(id)
-        }
-    }
-
-    function handleToggle(id: number){
-        setTodos(todos.map(todo =>
-            todo.id === id ? {... todo, completed : !todo.completed} : todo))
-    }
 
     return (
     <div className={"app"} id = "root">
-        <div className={"mainContainer"}>
-            <h1> Simple Todo App</h1>
-            <TodoForm onAdd={handleAdd}></TodoForm>
-            <TodoList todos={todos} onToggle={handleToggle} onDelete={handleDelete}></TodoList>
-        </div>
+        <Title className={"CS3733-D25"} teamNumber = {68} hobbies = {["Refurbishing Computers For Charity","FILL ME IN"]}></Title>
     </div>
     )
 }
---
+++ src/title.tsx
@@ -0,0 +1,29 @@
+type team = {
+    className: string
+    teamNumber: number
+    hobbies: string[]
+}
+
+//formats the hobbies to look good
+function getHobbies(hobbies: string[]) {
+    let s: string = ""
+    for(let i = 0; i < hobbies.length; i++) {
+        if(i == hobbies.length-2) {
+            s+= (hobbies[i]+", and ")
+        }
+        else if(i<hobbies.length-1) {
+            s+=(hobbies[i]+", ")
+        }
+        else{
+            s+=hobbies[i]
+        }
+    }
+    return s
+}
+
+
+export default function Title({className, teamNumber, hobbies}: team) {
+    return(
+        <h1>{className} Team {teamNumber} : {getHobbies(hobbies)}</h1>
+    )
+}
