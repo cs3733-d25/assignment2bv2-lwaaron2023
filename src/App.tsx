1632d008c501177811fab6a17609e5ef10d35bbe
lwaaron2023 <alwaller@wpi.edu>
2025-03-30 19:57:51 -0400

    Added functionality to form

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import Title from "./title.tsx";
 import Intro from "./intro.tsx";
 import List from "./list.tsx";
-import Table from "./table.tsx";
+import Form from "./form.tsx";
 
 
 
@@ -13,9 +13,12 @@ export default function App() {
     //Need to add this in a list: "1. Playing video games", "2. Playing the tenor saxophone", "3. Refurbishing computers for Charity"
     return (
     <div className={"app"} id = "root">
+        <div className={"segment"}>
         <Title className={"CS3733-D25"} teamNumber = {68} hobbies = {["Refurbishing Computers For Charity","FILL ME IN"]}></Title>
+        </div>
         <hr/>
-        <Intro person = {"Aaron"} paragraph={
+        <div className={"segment"}>
+        <Intro person = {"Aaron Waller"} paragraph={
             ["My name is Aaron Waller, and I am a sophomore at WPI.",
                 "I am currently studying for a Computer Science major.",
                 "I am from Needham, MA. I graduated from Needham High School in 2023.",
@@ -28,17 +31,49 @@ export default function App() {
             "My older brother is 23 years old, and my younger brother is 15 years old.",
             "My older brother is a BU graduate, and my younger brother is currently a freshman at Needham High School."
         ]}></Intro>
+        </div>
         <hr/>
-        <Intro person={"FILL ME IN"} paragraph={[
+        <div className={"segment"}>
+        <Intro person={"Haotian Lui"} paragraph={[
             "FILL",
             "ME",
             "IN"
         ]}></Intro>
+        </div>
         <hr/>
         <br/>
-        <Table rows={[["","Price","Quantity"],["Burger", "2.75","200"],["Fries", "2.50", "200"]]} headerRow={0} headerCol={0}></Table>
+        <div className={"segment"}>
+
+        </div>
     </div>
     )
 }
 
+/*
 
+{"orderings" ={["label","","input","","","label","","input","","","paragraph","input","label","","input","label","","input","label","","paragraph","input","label","","input","label","","input","label","","label","select","","","label","","","textarea","","","button"]}
+            "paragraphs" ={[{"text":"Purpose of inquiry:"},{"text":"Preferred method of contact:"}]}
+            "buttons"={[{"type":"submit","value":"Submit","text":"Submit"}]}
+            "inputs"={[{"type":"text","id":"fullname","name":"","value":""},
+                  {"type":"text","id":"email","name":"","value":""},
+                  {"type":"checkbox","id":"learn","name":"","value":""},
+                  {"type":"checkbox","id":"repairs","name":"","value":""},
+                  {"type":"checkbox","id":"recommendations","name":"","value":""},
+                  {"type":"radio","id":"first","name":"preference","value":"email"},
+                  {"type":"radio","id":"second","name":"preference","value":"phone"},
+                  {"type":"radio","id":"third","name":"preference","value":"other"}]}
+            "labels"={[{"intended":"fullname","text":"Enter your full name:"},
+                  {"intended":"email","text":"Enter your email:"},
+                  {"intended":"learn","text":"Learn about my other hobbies"},
+                  {"intended":"repairs","text":"Computer repairs"},
+                  {"intended":"recommendations","text":"Tech recommendations"},
+                  {"intended":"first","text":"Email"},
+                  {"intended":"second","text":"Phone"},
+                  {"intended":"third","text":"Other"},
+                  {"intended":"donations","text":"Do you have computers to donate:"},
+                  {"intended":"comments","text":"Additional Comments:"}]}
+
+              "selects"={[{"name":"donations","id":"donations",options:[{"value":"yes","text":"Yes"},{"value":"no","text":"No"}]}]}
+              "textareas"={[{"rows":10,"cols":40,"id":"comments"}]}}
+
+ */
--- src/form.tsx
+++ src/form.tsx
@@ -1,15 +1,124 @@
-
-type form = {
-    //header row is the row that is considered the header, while header column is the column that is considered the header
-    //set to -1 to indicate no header
-
-    rows: string[][]
-    headerRow: number
-    headerCol: number
-    ids: string[]
+type label = {
+    intended: string
+    text: string
 }
 
-export default function Form(){
+type input = {
+    type: string
+    name: string
+    value: string
+    id: string
+}
 
+type select = {
+    name: string
+    id: string
+    options: option[]
+}
 
+type option = {
+    value: string
+    text: string
+}
+
+type button = {
+    type: string
+    value: string
+    text: string
+}
+
+type textarea = {
+    rows: number
+    cols: number
+    id: string
+}
+
+type paragraph = {
+    text: string
+}
+
+type form = {
+    /*
+    Forms include:
+    1) label
+    2) input
+    3) p
+    4) select
+    5) textarea
+    6) button
+     */
+    orderings: string[] //a list of the element types in the order they appear in the form
+    labels: label[] //all the labels
+    inputs: input[] //all the inputs
+    selects: select[] //all the selects
+    buttons: button[] //all the buttons
+    textareas: textarea[] //all the text areas
+    paragraphs: paragraph[] //all the paragraphs
+}
+
+export default function Form({orderings, labels, inputs, selects, buttons, textareas, paragraphs}:form){
+    const temp = []
+    const indices = [0,0,0,0,0,0]
+    //if the code doesn't recognize the tag it will put a <br/> tag
+    for(let i = 0; i < orderings.length; i++){
+        switch(orderings[i]){
+            case "label":
+                if(indices[0] < labels.length) {
+                    const l = labels[indices[0]]
+                    temp[i] = <label htmlFor={l.intended}>{l.text}</label>
+                    indices[0]++
+                }
+                break
+
+            case "input":
+                if(indices[1] < inputs.length) {
+                    const inp = inputs[indices[1]]
+                    temp[i] = <input type={inp.type} name ={inp.name} value = {inp.value} id = {inp.id}></input>
+                    indices[1]++
+                }
+                break
+
+            case "select":
+                if(indices[2] < selects.length) {
+                    const s = selects[indices[2]]
+                    const temp2 = []
+                    for(let j = 0; j < s.options.length; j++){
+                        temp2[j] = <option value = {s.options[j].value}>{s.options[j].text}</option>
+                    }
+                    temp[i] = <select name = {s.name} id = {s.id}>{temp2}</select>
+                    indices[2]++
+                }
+                break
+
+            case "button":
+                if(indices[3] < buttons.length) {
+                    const b = buttons[indices[3]]
+                    temp[i] = <button type = {(b.type === "submit" ||b.type === "reset" || b.type === "button")?b.type:undefined} value={b.value} >{b.text}</button>
+                    indices[3]++
+                }
+                break
+
+            case "textarea":
+                if(indices[4] < textareas.length) {
+                    const ta = textareas[indices[4]]
+                    temp[i] = <textarea rows ={ta.rows} cols ={ta.cols} id={ta.id}></textarea>
+                    indices[4]++
+                }
+                break
+
+            case "paragraph":
+                if(indices[5] < paragraphs.length) {
+                    const p = paragraphs[indices[5]]
+                    temp[i] = <p>{p.text}</p>
+                    indices[5]++
+                }
+                break
+
+            default:
+                temp[i] = <br/>
+        }
+    }
+    return (<div className={"Form-container"}>
+        <form className={"Form"}>{temp}</form>
+    </div>)
 }
